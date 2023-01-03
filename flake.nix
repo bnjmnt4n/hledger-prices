1a8d2400e51df1e48bd2b8b45be5249bb35db435
Benjamin Tan <benjamin@dev.ofcr.se>
2023-01-03 16:56:01 +0800

    Add Nix flake files

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?rev=04f574a1c0fde90b51bf68198e2297ca4e7cccf4";
+    flake-utils.url = "github:numtide/flake-utils?rev=5aed5285a952e0b949eb3ba02c12fa4fcfef535f";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages."${system}";
+        hledger-prices = pkgs.buildGoModule {
+          pname = "hledger-prices";
+          version = "0.0.0";
+          src = pkgs.lib.cleanSource ./.;
+          vendorHash = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+        };
+      in
+      {
+        packages.default = hledger-prices;
+        apps.default = flake-utils.lib.mkApp {
+          drv = hledger-prices;
+        };
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            pkgs.go
+            pkgs.gopls
+          ];
+        };
+      });
+}
